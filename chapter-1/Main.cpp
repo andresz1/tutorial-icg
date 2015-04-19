38717ce90db4fadde9a47456655f01c5404bfe30
andresz1 <a.alvarez.sor@gmail.com>
2015-04-19 12:17:47 -0430

    Update chapter-1

--- chapter-1/Main.cpp
+++ chapter-1/Main.cpp
@@ -1,14 +1,66 @@
 #include "Main.h"
 #include "Line.h"
 #include "Quad.h"
+#include "UserInterface.h"
 
 using std::vector;
 
 GLFWwindow *gWindow;
 int gWidth, gHeight;
 bool gPress;
+CUserInterface * userInterface;
 vector <CFigure *> figures;
 FigureType figureSelected;
+int picked;
+
+void pick(int x, int y)
+{
+	picked = -1;
+	userInterface->hide();
+
+	for (int i = 0; i < figures.size(); i++)
+	{
+		float *v1 = figures[i]->getVertex(0);
+		float *v2 = figures[i]->getVertex(1);
+		float max[2];
+		float min[2];
+
+		// This should be precalculated
+
+		max[0] = MAX(v1[0], v2[0]);
+		max[1] = MAX(v1[1], v2[1]);
+
+		min[0] = MIN(v1[0], v2[0]);
+		min[1] = MIN(v1[1], v2[1]);
+
+
+		if (x >= min[0] && x <= max[0] && y >= min[1] && y <= max[1])
+		{
+			picked = i;
+
+			userInterface->setFigureColor(figures[picked]->getColor());
+			userInterface->show();
+
+			int type = figures[picked]->getType();
+
+			if (type == LINE)
+				userInterface->setFigureType("Line");
+			else
+				userInterface->setFigureType("Quad");
+
+			break;
+		}
+	}
+}
+
+void updateUserInterface()
+{
+	if (picked > -1)
+	{
+		float * color = userInterface->getFigureColor();
+		figures[picked]->setColor(color[0], color[1], color[2]);
+	}
+}
 
 void display()
 {
@@ -26,59 +78,81 @@ void reshape(GLFWwindow *window, int width, int height)
 
 	glViewport(0, 0, gWidth, gHeight);
 
+	userInterface->reshape();
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0f, gWidth, 0.0f, gHeight, -1.0f, 1.0f);
 
-	glMatrixMode(GL_MODELVIEW);
+	glOrtho(0, gWidth, 0, gHeight, -1.0f, 1.0f);
 }
 
 void keyInput(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
+	if (TwEventKeyGLFW(key, action))
+		return;
+
 	if (action == GLFW_PRESS)
 	{
 		switch (key)
 		{
-			case GLFW_KEY_ESCAPE:
-				glfwSetWindowShouldClose(window, GL_TRUE);
-				break;
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			break;
 
-			case GLFW_KEY_L:
-				figureSelected = LINE;
-				break;
+		case GLFW_KEY_P:
+			figureSelected = NONE;
+			userInterface->hide();
+			break;
 
-			case GLFW_KEY_Q:
-				figureSelected = QUAD;
-				break;
+		case GLFW_KEY_L:
+			figureSelected = LINE;
+			userInterface->hide();
+			break;
+
+		case GLFW_KEY_Q:
+			figureSelected = QUAD;
+			userInterface->hide();
+			break;
 		}
 	}
 }
 
 void mouseButton(GLFWwindow* window, int button, int action, int mods)
 {
+	if (TwEventMouseButtonGLFW(button, action))
+		return;
+
 	double x, y;
 	glfwGetCursorPos(gWindow, &x, &y);
 
 	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS && !gPress)
 	{
+		float ax = x;
 		float ay = gHeight - y;
 
-		if (figureSelected == LINE)
+		if (figureSelected == NONE)
+		{
+			pick(ax, ay);
+			printf("%d\n", picked);
+		}
+		else if (figureSelected == LINE)
 		{
 			CLine *line = new CLine();
-			line->setVertex(0, x, ay);
-			line->setVertex(1, x, ay);
+			line->setVertex(0, ax, ay);
+			line->setVertex(1, ax, ay);
 			figures.push_back(line);
+
+			gPress = true;
 		}
-		else if (figureSelected == QUAD)
+		else
 		{
 			CQuad *quad = new CQuad();
-			quad->setVertex(0, x, ay);
-			quad->setVertex(1, x, ay);
+			quad->setVertex(0, ax, ay);
+			quad->setVertex(1, ax, ay);
 			figures.push_back(quad);
-		}
 
-		gPress = true;
+			gPress = true;
+		}
 	}
 
 	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
@@ -87,12 +161,30 @@ void mouseButton(GLFWwindow* window, int button, int action, int mods)
 
 void cursorPos(GLFWwindow* window, double x, double y)
 {
+	if (TwEventMousePosGLFW(x, y))
+		return;
+
 	if (gPress)
-		figures.back()->setVertex(1, x, gHeight - y);
+	{
+		float ax = x;
+		float ay = gHeight - y;
+
+		figures.back()->setVertex(1, ax, ay);
+	}
+}
+
+void charInput(GLFWwindow* window, static unsigned int scanChar)
+{
+	if (TwEventCharGLFW(scanChar, GLFW_PRESS))
+		return;
 }
 
 void destroy()
 {
+	for (int i = 0; i < figures.size(); i++)
+		delete figures[i];
+
+	TwTerminate();
 	glfwDestroyWindow(gWindow);
 	glfwTerminate();
 }
@@ -119,7 +211,18 @@ bool initGlfw()
 	glfwSetKeyCallback(gWindow, keyInput);
 	glfwSetMouseButtonCallback(gWindow, mouseButton);
 	glfwSetCursorPosCallback(gWindow, cursorPos);
-	
+	glfwSetCharCallback(gWindow, charInput);
+
+	return true;
+}
+
+bool initUserInterface()
+{
+	if (!TwInit(TW_OPENGL, NULL))
+		return false;
+
+	userInterface = new CUserInterface();
+
 	return true;
 }
 
@@ -129,26 +232,30 @@ int main(void)
 	gHeight = 768;
 	gPress = false;
 	figureSelected = LINE;
+	picked = -1;
 
-	if (!initGlfw())
+	if (!initGlfw() || !initUserInterface())
 		return EXIT_FAILURE;
+
+	reshape(gWindow, gWidth, gHeight);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
-	reshape(gWindow, gWidth, gHeight);
 
 	while (!glfwWindowShouldClose(gWindow))
 	{
 		display();
 
+		TwDraw();
+
+		updateUserInterface();
+
 		glfwSwapBuffers(gWindow);
 
 		glfwPollEvents();
 	}
-	
+
 	destroy();
 
 	return EXIT_SUCCESS;
 }
-
--- chapter-1/Main.h
+++ chapter-1/Main.h
@@ -1,7 +1,12 @@
 #include "../headers/glfw3.h"
-#include <iostream>
-#include <string>
 #include <vector>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+void pick(int x, int y);
+
+void updateUserInterface();
 
 void display();
 
@@ -13,8 +18,12 @@ void mouseButton(GLFWwindow* window, int button, int action, int mods);
 
 void cursorPos(GLFWwindow* window, double x, double y);
 
+void charInput(GLFWwindow* window, static unsigned int scanChar);
+
 void destroy();
 
 bool initGlfw();
+
+bool initUserInterface();
 
 int main(void);
